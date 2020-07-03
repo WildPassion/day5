f347c1ff6fc27f063c4216f7f03996f9abfc1a5d
WildDed <ant4ded@gmail.com>
2020-07-03 18:03:06 +0300

    Added read from console

--- src/main/java/by/epam/dedik/day5/reader/TextReader.java
+++ src/main/java/by/epam/dedik/day5/reader/TextReader.java
@@ -3,6 +3,7 @@ package by.epam.dedik.day5.reader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class TextReader {
@@ -14,6 +15,13 @@ public class TextReader {
         } catch (NullPointerException | IOException e) {
             throw new RuntimeException("Error opening file " + file, e);
         }
+        return data;
+    }
+
+    public String readFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.next();
+        scanner.close();
         return data;
     }
 }
